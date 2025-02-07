45bccf4ae088e38de907210fe02297b52fad11f1
Chris Hall <chrish@tectratech.com>
2025-02-07 16:50:52 +0000

    Fix Errors

--- src/components/StockDashboard/index.js
+++ src/components/StockDashboard/index.js
@@ -348,7 +348,7 @@ const StockDashboard = () => {
                   £{buyer === 'all' 
                     ? Object.values(buyerTotals).reduce((a, b) => a + b, 0)
                     : buyerTotals[buyer] || 0
-                  }
+                  ).toFixed(2)}
                 </div>
               </div>
             ))}
